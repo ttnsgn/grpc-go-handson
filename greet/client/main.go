e11ebbdcae0946455a32e7c5041c54725fac7113
ttnsgn <ttnsgnid@gmail.com>
2025-02-20 16:44:09 +0700

    feat(greet) done for greet example

---
+++ greet/client/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+
+	pb "github.com/ttnsgn/grpc-go-handson/greet/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var addr string = "localhost:50051"
+
+func main() {
+	conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to connect: %v\n", err)
+	}
+
+	defer conn.Close()
+	c := pb.NewGreetServiceClient(conn)
+
+	doGreet(c)
+}
