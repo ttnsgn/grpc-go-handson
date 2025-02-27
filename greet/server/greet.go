a2d4c8205fdfb92f73865f9592942186c0479953
ttnsgn <ttnsgnid@gmail.com>
2025-02-27 07:19:41 +0700

    add server stream implementation

--- greet/server/greet.go
+++ greet/server/greet.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	pb "github.com/ttnsgn/grpc-go-handson/greet/proto"
+	"google.golang.org/grpc"
 )
 
 func (s *Server) Greet(ctx context.Context, in *pb.GreetRequest) (*pb.GreetResponse, error) {
@@ -12,4 +14,18 @@ func (s *Server) Greet(ctx context.Context, in *pb.GreetRequest) (*pb.GreetRespo
 	return &pb.GreetResponse{
 		Result: "Hello " + in.FirstName,
 	}, nil
+}
+
+func (s *Server) GreetManyTimes(in *pb.GreetRequest, stream grpc.ServerStreamingServer[pb.GreetResponse]) error {
+	log.Printf("GreetManyTimes was invoked with: %v\n", in)
+	for i := range 10 {
+		res := fmt.Sprintf("Hello %s, number %d", in.FirstName, i)
+		err := stream.Send(&pb.GreetResponse{
+			Result: res,
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
