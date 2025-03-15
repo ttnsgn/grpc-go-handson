72e84aded9603a335fdfde02bca8b2646f5aa1c6
ttnsgn <ttnsgnid@gmail.com>
2025-03-15 23:18:38 +0700

    comment out unused func

--- calculator/client/calculator.go
+++ calculator/client/calculator.go
@@ -8,18 +8,18 @@ import (
 	pb "github.com/ttnsgn/grpc-go-handson/calculator/proto"
 )
 
-func doSum(c pb.CalculatorServiceClient) {
-	log.Println("func doSum was invoked")
-	res, err := c.Sum(context.Background(), &pb.SumRequest{
-		FisrtNumber: 2,
-		SecondNumer: 3,
-	})
-	if err != nil {
-		log.Fatalf("Could not do sum: %v\n", err)
-	}
-
-	log.Printf("Sum result: %d", res.Result)
-}
+// func doSum(c pb.CalculatorServiceClient) {
+// 	log.Println("func doSum was invoked")
+// 	res, err := c.Sum(context.Background(), &pb.SumRequest{
+// 		FisrtNumber: 2,
+// 		SecondNumer: 3,
+// 	})
+// 	if err != nil {
+// 		log.Fatalf("Could not do sum: %v\n", err)
+// 	}
+//
+// 	log.Printf("Sum result: %d", res.Result)
+// }
 
 func doPrime(c pb.CalculatorServiceClient) {
 	log.Println("func doPrime was invoked")
--- greet/client/greet.go
+++ greet/client/greet.go
@@ -8,17 +8,17 @@ import (
 	pb "github.com/ttnsgn/grpc-go-handson/greet/proto"
 )
 
-func doGreet(c pb.GreetServiceClient) {
-	log.Println("doGreet was invoked")
-	res, err := c.Greet(context.Background(), &pb.GreetRequest{
-		FirstName: "Tatan",
-	})
-	if err != nil {
-		log.Fatalf("Could not greet: %v\n", err)
-	}
-
-	log.Printf("Greeting: %s\n", res.Result)
-}
+// func doGreet(c pb.GreetServiceClient) {
+// 	log.Println("doGreet was invoked")
+// 	res, err := c.Greet(context.Background(), &pb.GreetRequest{
+// 		FirstName: "Tatan",
+// 	})
+// 	if err != nil {
+// 		log.Fatalf("Could not greet: %v\n", err)
+// 	}
+//
+// 	log.Printf("Greeting: %s\n", res.Result)
+// }
 
 func doGreetManyTimes(c pb.GreetServiceClient) {
 	log.Println("doGreetManyTimes was invoked")
